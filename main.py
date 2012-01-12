49c2ddc4e401afd2e3bc221fe4e0464e410f336f
minami <minami@pylone.jp>
2012-01-12 21:13:39 +0900

    -

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import gdbmi
 
 def main():
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
+        #level=logging.DEBUG,
         format='%(asctime)s '\
             '%(levelname)s '\
             '%(pathname)s:%(lineno)s '\
@@ -49,7 +50,7 @@ def main():
                     token = s.send('-var-delete ' + name)
                     while not s.block(token): pass
 
-                token = s.send('-var-create' + " - *" +  (" (*" +d['name'] + ")"),
+                token = s.send('-var-create - * (*' +d['name'] + ')',
                                got_var)
                 while not s.block(token): pass
         return True    
@@ -58,8 +59,6 @@ def main():
 
     # skip dummy() and return '1234'
     token = s.send("-exec-return 1234")
-    while not s.block(token): pass
-
     token = s.send("-exec-continue")
     while not s.block():
         if s.exec_state == "stopped":
@@ -70,30 +69,31 @@ def main():
         frame = stack['frame']
         for d in frame['args']:
             logging.warn([d['name'], d['value']])
-            if d['name'] == 'src':
+            if d['name'] == 'fds':
                 addr = int(d['value'], 16)
 
                 def got_var(token, obj):
                     name = obj.args['name']
                     logging.error(name)
-                    # set value of arg (*var) = 99
-                    s.send('-var-assign ' + name + " 99")
+                    s.send('-var-assign ' + name + " 1")
                     s.send('-var-update ' + name )
                     token = s.send('-var-delete ' + name)
                     while not s.block(token): pass
 
-                token = s.send('-var-create' + " - *" +  (" (*" +d['name'] + ")"),
+                token = s.send('-var-create - * fds[0].fd',
                                got_var)
                 while not s.block(token): pass
         return True    
 
 
-    token = s.send("-stack-list-arguments 1 0 0", tweak_args)
+    token = s.send("-stack-list-arguments 1 0 0", hijack_pool)
     while not s.block(token): pass
 
+    token = s.send("-exec-return 1")
+    while not s.block(token): pass
 
-
-    while not s.block(): pass
+    token = s.send("-exec-continue")
+    while not s.block(token): pass
 
 if __name__ == "__main__":
     main()
