7a626e66e2bba0912405fe3d3b095f566c6ad2ae
minami <minami@pylone.jp>
2012-01-20 19:29:53 +0900

    -

--- test/altsect.c
+++ test/altsect.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+enum test_op {
+    TEST_OP_TEAR_NONE = 0,
+    TEST_OP_TEAR_UP,
+    TEST_OP_TEAR_DOWN,
+    TEST_OP_ASSERT,
+};
+
+struct test_case {
+    enum test_op op;
+    
+};
+
+#define SECT "TESTCASES"
 int main(){
-     __attribute__((__section__(SECT))) static int alt_sect;
-    printf("%d\n", alt_sect);
+    __attribute__((used, __section__(SECT)))
+         static struct test_case test_xxx[] = \
+         {
+             {
+                 .op = TEST_OP_TEAR_UP,
+             }
+         };
     return 0;
 }
